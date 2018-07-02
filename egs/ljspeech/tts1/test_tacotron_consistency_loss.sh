53aff82c89d296fbafc74626f97a152566425a3f
Ramon Fernandez <ramon@astudillo.com>
2018-07-02 19:52:37 -0400

    Cleare example

--- egs/ljspeech/tts1/test_tacotron_consistency_loss.sh
+++ egs/ljspeech/tts1/test_tacotron_consistency_loss.sh
@@ -4,5 +4,5 @@
 
 # Wrapper testing the loss
 python test_tacotron_consistency_loss.py \
-        --model-conf model.conf \
+        --model-conf exp/train_no_dev_taco2_enc512-3x5x512-1x512_dec2x1024_pre2x256_post5x5x512_att128-15x32_cm_bn_cc_msk_pw20.0_do0.5_zo0.1_lr1e-3_ep1e-6_wd0.0_bs64_sd1125/results/model.conf \
         --train-json dump/eval/data.json
