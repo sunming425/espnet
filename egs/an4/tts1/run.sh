4758ae374a68f124ebc0afb6c355f3657d148941
Ramon Fernandez <ramon@astudillo.com>
2018-07-05 18:00:28 -0400

    WIP Add Tacotron training for AN4
    Recipe runs. Takes ~5h on JHU cluster. Still need to check results.
    
    Needed for https://github.com/espnet/JSALT18main/issues/49

---
+++ egs/an4/tts1/run.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+
+# Copyright 2018 Nagoya University (Tomoki Hayashi)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh
+. ./cmd.sh
+
+# genearl configuration
+stage=-1
+ngpu=1       # number of gpu in training
+nj=32        # numebr of parallel jobs
+dumpdir=dump # directory to dump full features
+verbose=0    # verbose option (if set > 1, get more log)
+seed=1       # random seed number
+resume=""    # the snapshot path to resume (if set empty, no effect)
+
+# feature extraction related
+fs=16000    # sampling frequency
+fmax=""     # maximum frequency
+fmin=""     # minimum frequency
+n_mels=80   # number of mel basis
+n_fft=1024  # number of fft points
+n_shift=512 # number of shift points
+# encoder related
+embed_dim=512
+elayers=1
+eunits=512
+econv_layers=3 # if set 0, no conv layer is used
+econv_chans=512
+econv_filts=5
+do_delta=false # true when using CNN (MUST BE ALLWAYS FALSE FOR TACOTRON)
+
+# decoder related
+dlayers=2
+dunits=1024
+prenet_layers=2  # if set 0, no prenet is used
+prenet_units=256
+postnet_layers=5 # if set 0, no postnet is used
+postnet_chans=512
+postnet_filts=5
+
+# attention related
+adim=128
+aconv_chans=32
+aconv_filts=15      # resulting in filter_size = aconv_filts * 2 + 1
+cumulate_att_w=true # whether to cumulate attetion weight
+use_batch_norm=true # whether to use batch normalization in conv layer
+use_concate=true    # whether to concatenate encoder embedding with decoder lstm outputs
+use_residual=false  # whether to concatenate encoder embedding with decoder lstm outputs
+use_masking=true    # whether to mask the padded part in loss calculation
+bce_pos_weight=20.0
+# minibatch related
+batchsize=64
+batch_sort_key="" # empty or input or output (if empty, shuffled batch will be used)
+maxlen_in=150     # if input length  > maxlen_in, batchsize is reduced (if batch_sort_key="", not effect)
+maxlen_out=400    # if output length > maxlen_out, batchsize is reduced (if batch_sort_key="", not effect)
+# optimization related
+lr=1e-3
+eps=1e-6
+weight_decay=0.0
+dropout=0.5
+zoneout=0.1
+epochs=200
+
+# decoding related
+model=model.loss.best
+threshold=0.5 # threshold to stop the generation
+maxlenratio=10.0
+minlenratio=0.0
+
+# root directory of db
+db_root=downloads
+an4_root=${db_root}/an4
+data_url=http://www.speech.cs.cmu.edu/databases/an4/
+
+# exp tag
+tag="" # tag for managing experiments.
+
+. utils/parse_options.sh || exit 1;
+
+# check gpu option usage
+if [ ! -z $gpu ]; then
+    echo "WARNING: --gpu option will be deprecated."
+    echo "WARNING: please use --ngpu option."
+    if [ $gpu -eq -1 ]; then
+        ngpu=0
+    else
+        ngpu=1
+    fi
+fi
+
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train_no_dev
+train_dev=train_dev
+recog_set="train_dev test"
+
+# AN4 CHANGE: Download an4 data
+if [ ${stage} -le -1 ]; then
+    echo "stage -1: Data Download"
+    mkdir -p ${db_root}
+    local/download_and_untar.sh ${db_root} ${data_url}
+fi
+
+# To reset this stage rm -R data exp
+if [ ${stage} -le 0 ]; then
+    ### Task dependent. You have to make data the following preparation part by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 0: Data preparation"
+    mkdir -p data/{train,test} exp
+
+    if [ ! -f ${an4_root}/README ]; then
+        echo Cannot find an4 root! Exiting...
+        exit 1
+    fi
+
+    python local/data_prep.py ${an4_root} ${KALDI_ROOT}/tools/sph2pipe_v2.5/sph2pipe
+
+    for x in test train; do
+        for f in text wav.scp utt2spk; do
+            sort data/${x}/${f} -o data/${x}/${f}
+        done
+        utils/utt2spk_to_spk2utt.pl data/${x}/utt2spk > data/${x}/spk2utt
+    done
+fi
+
+# AN4 CHANGE: an4 features 
+feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+if [ ${stage} -le 1 ]; then
+    ### Task dependent. You have to design training and dev sets by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 1: Feature Generation"
+    fbankdir=fbank
+    # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
+    for x in test train; do
+        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 8 data/${x} exp/make_fbank/${x} ${fbankdir}
+    done
+
+    # make a dev set
+    utils/subset_data_dir.sh --first data/train 100 data/${train_dev}
+    n=$[`cat data/train/text | wc -l` - 100]
+    utils/subset_data_dir.sh --last data/train ${n} data/${train_set}
+
+    # compute global CMVN
+    compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
+
+    # dump features
+    dump.sh --cmd "$train_cmd" --nj 8 --do_delta $do_delta \
+        data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
+    dump.sh --cmd "$train_cmd" --nj 9 --do_delta $do_delta \
+        data/${train_dev}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj 8 --do_delta $do_delta \
+            data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
+    done
+fi
+
+dict=data/lang_1char/${train_set}_units.txt
+echo "dictionary: ${dict}"
+if [ ${stage} -le 2 ]; then
+    ### Task dependent. You have to check non-linguistic symbols used in the corpus.
+    echo "stage 2: Dictionary and Json Data Preparation"
+    mkdir -p data/lang_1char/
+    echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
+    | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
+    wc -l ${dict}
+
+    # make json labels
+    data2json.sh --feat ${feat_tr_dir}/feats.scp \
+         data/${train_set} ${dict} > ${feat_tr_dir}/data.json
+    data2json.sh --feat ${feat_dt_dir}/feats.scp \
+         data/${train_dev} ${dict} > ${feat_dt_dir}/data.json
+    for rtask in ${recog_set}; do
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        data2json.sh --feat ${feat_recog_dir}/feats.scp \
+            data/${rtask} ${dict} > ${feat_recog_dir}/data.json
+    done
+fi
+
+
+if [ -z ${tag} ];then
+    expdir=exp/${train_set}_taco2_enc${embed_dim}
+    if [ ${econv_layers} -gt 0 ];then
+        expdir=${expdir}-${econv_layers}x${econv_filts}x${econv_chans}
+    fi
+    expdir=${expdir}-${elayers}x${eunits}_dec${dlayers}x${dunits}
+    if [ ${prenet_layers} -gt 0 ];then
+        expdir=${expdir}_pre${prenet_layers}x${prenet_units}
+    fi
+    if [ ${postnet_layers} -gt 0 ];then
+        expdir=${expdir}_post${postnet_layers}x${postnet_filts}x${postnet_chans}
+    fi
+    expdir=${expdir}_att${adim}-${aconv_filts}x${aconv_chans}
+    if ${cumulate_att_w};then
+        expdir=${expdir}_cm
+    fi
+    if ${use_batch_norm};then
+        expdir=${expdir}_bn
+    fi
+    if ${use_residual};then
+        expdir=${expdir}_rs
+    fi
+    if ${use_concate};then
+        expdir=${expdir}_cc
+    fi
+    if ${use_masking};then
+        expdir=${expdir}_msk_pw${bce_pos_weight}
+    fi
+    expdir=${expdir}_do${dropout}_zo${zoneout}_lr${lr}_ep${eps}_wd${weight_decay}_bs$((batchsize*ngpu))
+    if [ ! -z ${batch_sort_key} ];then
+        expdir=${expdir}_sort_by_${batch_sort_key}_mli${maxlen_in}_mlo${maxlen_out}
+    fi
+    expdir=${expdir}_sd${seed}
+else
+    expdir=exp/${train_set}_${tag}
+fi
+
+if [ ${stage} -le 3 ];then
+    echo "stage 3: Text-to-speech model training"
+    tr_json=${feat_tr_dir}/data.json
+    dt_json=${feat_dt_dir}/data.json
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        tts_train.py \
+           --ngpu ${ngpu} \
+           --outdir ${expdir}/results \
+           --verbose ${verbose} \
+           --seed ${seed} \
+           --resume ${resume} \
+           --train-json ${tr_json} \
+           --valid-json ${dt_json} \
+           --embed_dim ${embed_dim} \
+           --elayers ${elayers} \
+           --eunits ${eunits} \
+           --econv_layers ${econv_layers} \
+           --econv_chans ${econv_chans} \
+           --econv_filts ${econv_filts} \
+           --dlayers ${dlayers} \
+           --dunits ${dunits} \
+           --prenet_layers ${prenet_layers} \
+           --prenet_units ${prenet_units} \
+           --postnet_layers ${postnet_layers} \
+           --postnet_chans ${postnet_chans} \
+           --postnet_filts ${postnet_filts} \
+           --adim ${adim} \
+           --aconv-chans ${aconv_chans} \
+           --aconv-filts ${aconv_filts} \
+           --cumulate_att_w ${cumulate_att_w} \
+           --use_batch_norm ${use_batch_norm} \
+           --use_concate ${use_concate} \
+           --use_residual ${use_residual} \
+           --use_masking ${use_masking} \
+           --bce_pos_weight ${bce_pos_weight} \
+           --lr ${lr} \
+           --eps ${eps} \
+           --dropout-rate ${dropout} \
+           --zoneout-rate ${zoneout} \
+           --weight-decay ${weight_decay} \
+           --batch_sort_key ${batch_sort_key} \
+           --batch-size ${batchsize} \
+           --maxlen-in ${maxlen_in} \
+           --maxlen-out ${maxlen_out} \
+           --epochs ${epochs}
+fi
+
+exit
+
+outdir=${expdir}/outputs_${model}_th${threshold}_mlr${minlenratio}-${maxlenratio}
+if [ ${stage} -le 4 ];then
+    echo "stage 4: Decoding"
+    for sets in ${train_dev} ${eval_set};do
+        [ ! -e  ${outdir}/${sets} ] && mkdir -p ${outdir}/${sets}
+        cp ${dumpdir}/${sets}/data.json ${outdir}/${sets}
+        splitjson.py --parts ${nj} ${outdir}/${sets}/data.json
+        # decode in parallel
+        ${train_cmd} JOB=1:$nj ${outdir}/${sets}/log/decode.JOB.log \
+            tts_decode.py \
+                --backend pytorch \
+                --ngpu 0 \
+                --verbose ${verbose} \
+                --out ${outdir}/${sets}/feats.JOB \
+                --json ${outdir}/${sets}/split${nj}utt/data.JOB.json \
+                --model ${expdir}/results/${model} \
+                --model-conf ${expdir}/results/model.conf \
+                --threshold ${threshold} \
+                --maxlenratio ${maxlenratio} \
+                --minlenratio ${minlenratio}
+        # concatenate scp files
+        for n in $(seq $nj); do
+            cat "${outdir}/${sets}/feats.$n.scp" || exit 1;
+        done > ${outdir}/${sets}/feats.scp
+    done
+fi
+
+if [ ${stage} -le 5 ];then
+    echo "stage 5: Synthesis"
+    for sets in ${train_dev} ${eval_set};do
+        [ ! -e ${outdir}_denorm/${sets} ] && mkdir -p ${outdir}_denorm/${sets}
+        apply-cmvn --norm-vars=true --reverse=true data/${train_set}/cmvn.ark \
+            scp:${outdir}/${sets}/feats.scp \
+            ark,scp:${outdir}_denorm/${sets}/feats.ark,${outdir}_denorm/${sets}/feats.scp
+        local/convert_fbank.sh --nj ${nj} --cmd "${train_cmd}" \
+            --fs ${fs} --fmax "${fmax}" --fmin "${fmin}" \
+            --n_mels ${n_mels} --n_fft ${n_fft} --n_shift ${n_shift} \
+            ${outdir}_denorm/${sets} \
+            ${outdir}_denorm/${sets}/log \
+            ${outdir}_denorm/${sets}/wav
+    done
+fi
